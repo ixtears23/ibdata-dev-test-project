bea7720d509d0bb500635a215a594d79c6dbd427
ixtears23 <ixtears23@icloud.com>
2019-05-25 01:12:42 +0900

    Controller Test 추가

---
+++ ibdata-dev-test-project/src/test/java/ibdata/com/tdd/main/HomeControllerTest.java
@@ -0,0 +1,46 @@
+package ibdata.com.tdd.main;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import ibdata.com.tdd.config.web.DispatcherConfig;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration(classes = DispatcherConfig.class)
+public class HomeControllerTest {
+	
+	@Autowired
+	private WebApplicationContext wac;
+	
+	MockMvc mockMvc;
+	
+	@Before
+	public void setup() {
+		this.mockMvc =  MockMvcBuilders.webAppContextSetup(this.wac).build();
+	}
+	
+	@Test
+	public void shouldHomeUrlRequestIsOkAndViewNameIsHome() throws Exception {
+		
+		mockMvc.perform(get("/{home}.do", "home"))
+				.andDo(print())
+			.andExpect(view().name("home"))
+			.andExpect(status().isOk())
+			.andReturn();
+		
+	} 
+}
